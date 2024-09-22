8287231ba632367f45f2b816affecc6d762262e8
H82M19\Student <Student>
2024-09-22 10:50:49 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [diceRolls, setDiceRolls] = useState([]);
+
+  const rollDice = () => {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    setDiceRolls(prevRolls => [...prevRolls, randomNumber]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={rollDice}>Roll Dice</button>
+      <ul>
+        {diceRolls.map((roll, index) => (
+          <li key={index}>{roll}</li>
+        ))}
+      </ul>
     </div>
   );
 }
